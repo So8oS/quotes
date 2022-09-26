7937b2c83e2461ff10b63d12f28f8fb6143d5c31
So8oS <ahmadothman033@gmail.com>
2022-09-26 23:29:19 +0300

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,22 @@
 import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './main.css'
 import axios from 'axios'
 
 const Main = () => {
 const [data , setData] = useState([]);
-const [num,setNum] = useState(Math.floor(Math.random() * data.length));
+const [num,setNum] = useState(Math.floor(Math.random() * 1643));
 
 
 
-// useEffect(() => {
-//   const fetchData = async () =>{
-//         const {data: response} = await axios.get("https://type.fit/api/quotes");
-//         setData(response);
-//       }
-//       fetchData();
+  useEffect(() => {
+      (async() =>{
+        await  axios.get("https://type.fit/api/quotes")
+        .then(res => setData(res.data)).catch(error => console.log(error))
+        })();
+    },[])
 
-
-    
-//     // axios.get("https://type.fit/api/quotes")
-//     //     .then(res => setData(res.data)
-//     }, []);
-
-useEffect(() => {
-
-  (async() => 
-  {
-   await  axios.get("https://type.fit/api/quotes")
-   .then(res => setData(res.data)).catch(error => console.log(error))
-
-  })();
-
-
-},[])
-
-
-
-    
     console.log(data)
-
 
 
   return (
@@ -47,7 +24,6 @@ useEffect(() => {
         <h1 className='title' >Random Quote Generator</h1>
         <div  className='container'>
             <div className='inner' >
-                {/* <h1 className='text'>"A man that does not spend time with his family cn never be a real man"</h1> */}
                 <h1 className='text' >{data[num]?.text}</h1>
                 <h3 className='author' >{`-${data[num]?.author}-`}</h3>
             </div>
@@ -55,12 +31,11 @@ useEffect(() => {
         <button onClick={()=>{
             setNum(Math.floor(Math.random() * data.length))
             }} className='btn' >Click for a new quote</button>
-        <h1>{num}</h1>
+        <p>Quote Number: {num}</p>
 
         <div className='footer'>
           <a href="https://type.fit/api/quotes" target="_blank">Used Api</a>
         </div>
-        
     </div>
   )
 }
